38e6ba917f9c4af54d25f3447781f2ca8ed32274
Matt Levy <ducksoupdev@gmail.com>
2021-12-17 05:21:11 +0000

    feat: add persist to export map

--- test/unit/base-persist-reload.spec.mjs
+++ test/unit/base-persist-reload.spec.mjs
@@ -5,11 +5,16 @@ import { createPersist } from '../../src/index.mjs'
 let persist
 
 test.beforeEach(t => {
+  const getItem = sinon.stub()
+  getItem.withArgs('test:lastUpdated').returns('10')
   globalThis.localStorage = {
     setItem: sinon.spy(),
-    getItem: sinon.spy(),
-    removeItem: sinon.spy()
+    removeItem: sinon.spy(),
+    getItem
   }
+})
+
+test('set state', t => {
   globalThis.performance = {
     getEntriesByType (type) {
       return [
@@ -21,30 +26,9 @@ test.beforeEach(t => {
   persist = createPersist('test', 'local', {
     clearOnReload: true
   })
-})
-
-test('set state', t => {
-  persist.setState({ test: 'test', test2: 'test2' })
-  t.is(globalThis.localStorage.setItem.callCount, 2)
-  t.deepEqual(globalThis.localStorage.setItem.getCall(0).args, ['test:state', JSON.stringify({ test: 'test' })])
-  t.is(globalThis.localStorage.setItem.getCall(1).args[0], 'test:lastUpdated')
-})
-
-test('set empty state', t => {
-  persist.setState()
+  t.truthy(globalThis.localStorage.getItem.called)
+  t.is(globalThis.localStorage.getItem.getCall(0).args[0], 'test:lastUpdated')
   t.truthy(globalThis.localStorage.removeItem.called)
   t.is(globalThis.localStorage.removeItem.getCall(0).args[0], 'test:state')
   t.is(globalThis.localStorage.removeItem.getCall(1).args[0], 'test:lastUpdated')
-})
-
-test('get state', t => {
-  persist.getState()
-  t.truthy(globalThis.localStorage.getItem.called)
-  t.is(globalThis.localStorage.getItem.getCall(0).args[0], 'test:state')
-})
-
-test('get lastUpdated', t => {
-  persist.lastUpdated()
-  t.truthy(globalThis.localStorage.getItem.called)
-  t.is(globalThis.localStorage.getItem.getCall(1).args[0], 'test:lastUpdated')
 })
