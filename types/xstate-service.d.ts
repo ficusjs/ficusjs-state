f512c439f714fbe2ab98ff4e1365d225bfb4c2b8
Matt Levy <ducksoupdev@gmail.com>
2022-11-07 15:57:14 +0000

    fix: missing interpret type

--- types/xstate-service.d.ts
+++ types/xstate-service.d.ts
@@ -1,4 +1,4 @@
-import { StateMachine, EventObject, Typestate } from '@xstate/fsm'
+import { StateMachine, EventObject, Typestate, assign, createMachine } from '@xstate/fsm'
 import { Persist } from './persist'
 
 export interface XStateEvent {
@@ -28,10 +28,12 @@ export interface XStateGetterTree<TContext extends object> {
 
 export declare function addXStateService<TContext extends object, TEvent extends EventObject, TState extends Typestate<TContext>> (key: string, service: XStateService<TContext, TEvent, TState>): XStateService<TContext, TEvent, TState>
 
-export declare function interpret<TContext extends object, TEvent extends EventObject, TState extends Typestate<TContext>> (machine: StateMachine.Machine<TContext, TEvent, TState>, getters?: XStateGetterTree<TContext>, persist?: string | Persist<TState>): XStateService<TContext, TEvent, TState>
-
 export declare function createXStateService<TContext extends object, TEvent extends EventObject, TState extends Typestate<TContext>> (key: string, machine: StateMachine.Machine<TContext, TEvent, TState>, getters?: XStateGetterTree<TContext>, persist?: string | Persist<TState>): XStateService<TContext, TEvent, TState>
 
 export declare function getXStateService<TContext extends object, TEvent extends EventObject, TState extends Typestate<TContext>> (key: string): XStateService<TContext, TEvent, TState>
 
+export declare function interpret<TContext extends object, TEvent extends EventObject, TState extends Typestate<TContext>> (machine: StateMachine.Machine<TContext, TEvent, TState>, getters?: XStateGetterTree<TContext>, persist?: string | Persist<TState>): XStateService<TContext, TEvent, TState>
+
 export declare function wrapXStateService<TContext extends object, TEvent extends EventObject, TState extends Typestate<TContext>> (service: StateMachine.Service<TContext, TEvent, TState>, getters?: XStateGetterTree<TContext>, persist?: string | Persist<TState>): XStateService<TContext, TEvent, TState>
+
+export { assign, createMachine }
